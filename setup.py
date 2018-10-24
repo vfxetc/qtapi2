1a2603166720b47db1904626cfa61074ca76be32
Mike Boers <mikeb@loftysky.com>
2018-10-24 14:45:36 -0400

    Rename to qtapi2.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='qt20',
+    name='qtapi2',
     version='1.0',
     description='Cross-implementation and version support for PyQt(4|5) and PySide(|2).',
-    url='https://github.com/vfxetc/qt20',
+    url='https://github.com/vfxetc/qtapi2',
     
     packages=find_packages(exclude=['build*', 'tests*']),
     include_package_data=True,
     
     author='Mike Boers',
-    author_email='python-qt20@mikeboers.com.com',
+    author_email='floss+qtapi2@vfxetc.com',
     license='BSD-3',
 
     classifiers=[
