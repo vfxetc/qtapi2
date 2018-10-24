65877c1350e667abeb8c4f520276923b2c891d58
Mike Boers <mikeb@loftysky.com>
2018-10-24 14:44:36 -0400

    Switch to using Qt.py for the hard work.

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='qtapi2',
-    version='1.0',
-    description='Cross-implementation and version support for PyQt(4|5) and PySide(|2).',
+    version='1.0.0',
+    description='Simplifying wrapper around Qt.py.',
     url='https://github.com/vfxetc/qtapi2',
     
     packages=find_packages(exclude=['build*', 'tests*']),
@@ -13,6 +13,10 @@ setup(
     author_email='floss+qtapi2@vfxetc.com',
     license='BSD-3',
 
+    install_requires='''
+        Qt.py
+    ''',
+    
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
